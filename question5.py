ea359a48058bb1290cf9460ee9348d3e0df98b0c
Nalajala Teja Sri <SYR00406@syrencloud.com>
2023-05-18 14:31:00 +0530

    changed in q5

--- question5.py
+++ question5.py
@@ -1,7 +1,9 @@
 #5. WAP to print first 5 number which is divided from 3 in given range 2001 to 2300
 count=0
-print("First 5 number which is divided from 3 in given range 2001 to 2300 : ")
-for i in range(2001,2300):
+num1=int(input("Enter the stating range: "))
+num2=int(input("ENter the ending range: "))
+print("First 5 number which is divided from 3 in given range",num1,"to", num2," : ")
+for i in range(num1,num2):
     if(i%3==0):
         print(i,end=" ")
         count+=1
